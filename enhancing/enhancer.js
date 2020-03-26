575ecb62a32747ea539e8be7ebbc4e5a4340d228
root <guyton.m.oriji.578@gmail.com>
2020-03-26 08:46:14 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,42 @@
 module.exports = {
   succeed,
-  fail,
-  repair,
   get,
+  repair,
+  fail,
 };
 
+
+
+
+function repair(item) {  
+   item.durability = 100
+  return item
+}
+
+
 function succeed(item) {
-  return { ...item };
-}
-
-function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
+  if(item.enhancement<20){
+    item.enhancement += 1
+  }else{
+    item.enhancement = 20
+  }
+  
   return { ...item };
 }
 
 function get(item) {
+  return { ...item };
+}
+
+
+
+function fail(item) {
+  if(item.enhancement<15){
+    item.durability -=5
+  }if(item.enhancement>=15){
+    item.durability -=10
+  }if(item.enhancement>16){
+    item.enhancement -=1
+  }
   return { ...item };
 }
